26b1ccf767278228a1c50feebf47ae1c2bfccf8c
Rodrigo Costa <rodrigo@tirrell.local>
2019-03-28 13:53:49 -0300

    Database correction

--- RestWithAsp.NET-core.UDEMY 03 - Database integration/RestWithAsp.NET-core.UDEMY/Services/Implementations/PersonServiceImpl.cs
+++ RestWithAsp.NET-core.UDEMY 03 - Database integration/RestWithAsp.NET-core.UDEMY/Services/Implementations/PersonServiceImpl.cs
@@ -1,63 +1,74 @@
-﻿using System;
+﻿using RestWithAsp.NET_core.UDEMY.Models.Context;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 namespace RestWithAsp.NET_core.UDEMY.Services.Implementations
 {
     public class PersonServiceImpl : IPersonService
     {
-        private volatile int count;
+        private MySQLContext _mySQLContext;
+
+        public PersonServiceImpl(MySQLContext mySQLContext)
+        {
+            _mySQLContext = mySQLContext;
+        }
 
         public Person Create(Person person)
         {
-            person.Id = IncrementAndGetId();
-            return person;
+            try
+            {
+                _mySQLContext.Add(person);
+                _mySQLContext.SaveChanges();
+                return person;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public void Delete(long id)
         {
+            var fromDb = _mySQLContext.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            if (fromDb != null)
+            {
+                _mySQLContext.Persons.Remove(fromDb);
+                _mySQLContext.SaveChanges();
+            }
         }
 
         public List<Person> FindAll()
         {
-            List<Person> people = new List<Person>();
-            for (long r = 1; r <= 8; r++)
-            {
-                people.Add(MockPerson(r));
-            }
-            return people;
+            return _mySQLContext.Persons.ToList();
         }
 
         public Person FindById(long id)
         {
-            return MockPerson(id);
+            return _mySQLContext.Persons.SingleOrDefault(p => p.Id.Equals(id));
         }
 
         public Person Update(Person person)
         {
+            if (!Exist(person.Id))
+                return new Person();
+            var personFromDb = _mySQLContext.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+            try
+            {
+                _mySQLContext.Entry(personFromDb).CurrentValues.SetValues(person);
+                _mySQLContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
             return person;
         }
 
-        private Person MockPerson()
+        private bool Exist(long? id)
         {
-            return MockPerson(IncrementAndGetId());
-        }
-
-        private Person MockPerson(long id)
-        {
-            return new Person
-            {
-                Id = id,
-                FirstName = "Person Name " + id,
-                LastName = "Person Last Name " + id,
-                Address = "Some address "+ id,
-                Gender = "Male"
-            };
-        }
-
-        private long IncrementAndGetId()
-        {
-            return Interlocked.Increment(ref count);
+            return _mySQLContext.Persons.Any(p => p.Id.Equals(id));
         }
     }
 }
--- RestWithAsp.NET-core.UDEMY 03 - Database integration/RestWithAsp.NET-core.UDEMY/db/script.sql
+++ RestWithAsp.NET-core.UDEMY 03 - Database integration/RestWithAsp.NET-core.UDEMY/db/script.sql
@@ -1,7 +1,8 @@
 CREATE TABLE persons (
-	Id INT(10) unsigned null default null,
-    FistName varchar(50) null default null,
+	Id INT(10) NOT NULL AUTO_INCREMENT,
+    FirstName varchar(50) null default null,
     LastName varchar(50) null default null,
     Address varchar(50) null default null,
-    Gender varchar(50) null default null
+    Gender varchar(50) null default null,
+	PRIMARY KEY (Id)
 ) ENGINE = InnoDB;
