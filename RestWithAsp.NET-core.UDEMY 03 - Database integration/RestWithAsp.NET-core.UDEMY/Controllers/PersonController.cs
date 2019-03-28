8012184039715b3a3e77146a8536a030651a322c
Rodrigo Costa <rodrigo@tirrell.local>
2019-03-28 10:55:16 -0300

    Initial commit - creating project

---
+++ RestWithAsp.NET-core.UDEMY 03 - Database integration/RestWithAsp.NET-core.UDEMY/Controllers/PersonController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestWithAsp.NET_core.UDEMY.Services;
+
+namespace RestWithAsp.NET_core.UDEMY.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : Controller
+    {
+
+        private IPersonService _personService;
+
+        public PersonController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        // GET api/person
+        [HttpGet]
+        public IActionResult Get()
+        {   
+            return Ok(_personService.FindAll());
+        }
+
+        // GET api/person/5
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personService.FindById(id);
+            if (person == null)
+                return NotFound();
+            return Ok(_personService.FindById(id));
+        }
+
+        // POST api/person
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if (person == null)
+                return BadRequest();
+            return new ObjectResult(_personService.Create(person));
+        }
+
+        // PUT api/person/5
+        [HttpPut]
+        public IActionResult Put([FromBody] Person person)
+        {
+            if (person == null)
+                return BadRequest();
+            return new ObjectResult(_personService.Update(person));
+        }
+
+        // DELETE api/person/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ RestWithAsp.NET-core.UDEMY 03 - Database integration/RestWithAsp.NET-core.UDEMY/db/script.sql
@@ -0,0 +1,7 @@
+CREATE TABLE persons (
+	Id INT(10) unsigned null default null,
+    FistName varchar(50) null default null,
+    LastName varchar(50) null default null,
+    Address varchar(50) null default null,
+    Gender varchar(50) null default null
+) ENGINE = InnoDB;
