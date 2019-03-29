82f7fcda548c8f3215172ea03e53ca48f2ba41ed
Rodrigo Costa <rodrigo@tirrell.local>
2019-03-29 09:42:16 -0300

    Start project with dependecies

---
+++ RestWithAsp.NET-core.UDEMY 06 - Migration/RestWithAsp.NET-core.UDEMY/Startup.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RestWithAsp.NET_core.UDEMY.V1.Model.Context;
+using RestWithAsp.NET_core.UDEMY.V1.Repository;
+using RestWithAsp.NET_core.UDEMY.V1.Repository.Implementation;
+using RestWithAsp.NET_core.UDEMY.V1.Service;
+using RestWithAsp.NET_core.UDEMY.V1.Service.Implementation;
+
+namespace RestWithAsp.NET_core.UDEMY
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Database connection
+            var connection = Configuration["MySqlConnection:MySqlConnectionString"];
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
+
+            // Default .NET MVC
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // API Versioning
+            services.AddApiVersioning();
+
+            // Dependency Injection
+            services.AddScoped<IPersonService, PersonServiceImpl>();
+            services.AddScoped<IPersonRepository, PersonRepositoryImpl>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
